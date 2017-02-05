aa25d3c138e02ae68bfac629f132b2fbd594c99a
Thomas Li <lith@win.eng.vzwnet.com>
2017-02-04 22:54:56 -0500

    'first'

---
+++ build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["hi","index"],envversion:50,filenames:["hi.txt","index.txt"],objects:{},objnames:{},objtypes:{},terms:{hiii:0,index:1,modul:1,page:1,search:1,thi:[],thoma:[]},titles:["&lt;no title&gt;","Welcome to Note&#8217;s documentation!"],titleterms:{document:1,indic:1,note:1,tabl:1,welcom:1}})
